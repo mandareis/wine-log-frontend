518c472f82d72653a0c60afcad33191e2f17cca9
mandareis <amanda@depaula.xyz>
2020-12-02 14:48:28 -0800

    creates back/foward buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 (function () {
-  const loginBtn = document.querySelector("#login-btn");
-  const createWineBtn = document.querySelector("#wine-create-btn");
-
+  //   const loginBtn = document.querySelector("#login-btn");
+  //   const createWineBtn = document.querySelector("#wine-create-btn");
   const mainLeft = document.querySelector("#main-left");
   const mainCenter = document.querySelector("#main-center");
   const mainRight = document.querySelector("#main-right");
@@ -17,12 +16,6 @@
   login.addEventListener("click", () => {
     showLogin();
   });
-
-  function handlesLoginBtn() {
-    let userData = document.querySelector("#user");
-    let userName = userData.value;
-    createUser(userName);
-  }
 
   function showForm() {
     mainCenter.innerHTML = `
@@ -72,6 +65,21 @@
       .querySelector("#wine-create-form")
       .addEventListener("submit", handlesWineCreateForm);
   }
+
+  function handlesWineCreateForm() {
+    let wineName = document.querySelector("#wine-name");
+    let name = wineName.value;
+    let wineYear = document.querySelector("#wine-year");
+    let year = wineYear.value;
+    let wineKind = document.querySelector("#wine-kind");
+    let kind = wineKind.value;
+    let wineCost = document.querySelector("#wine-cost");
+    let cost = wineCost.value;
+    let wineReg = document.querySelector("#wine-reg");
+    let reg = wineReg.value;
+    createWine({ name, year, kind, cost, reg });
+  }
+
   function showUserSidebar() {
     mainLeft.innerHTML = `
         <div class="alert alert-success">
@@ -99,18 +107,10 @@
       .addEventListener("click", handlesLoginBtn);
   }
 
-  function handlesWineCreateForm() {
-    let wineName = document.querySelector("#wine-name");
-    let name = wineName.value;
-    let wineYear = document.querySelector("#wine-year");
-    let year = wineYear.value;
-    let wineKind = document.querySelector("#wine-kind");
-    let kind = wineKind.value;
-    let wineCost = document.querySelector("#wine-cost");
-    let cost = wineCost.value;
-    let wineReg = document.querySelector("#wine-reg");
-    let reg = wineReg.value;
-    createWine({ name, year, kind, cost, reg });
+  function handlesLoginBtn() {
+    let userData = document.querySelector("#user-name");
+    let userName = userData.value;
+    createUser(userName);
   }
 
   async function createUser(userName) {
@@ -140,19 +140,77 @@
     // data is our wine object, since the response was ".ok"!
     showWine(data);
   }
+
+  function apiURL(path) {
+    return `http://localhost:3000${path}`;
+  }
+  let pageNumber = 1;
+  async function getAllWines() {
+    let response = await fetch(apiURL(`/wines?page=${pageNumber}`));
+    let data = await response.json();
+    if (!response.ok) {
+      showError(data.error);
+      return;
+    }
+    // data is our wine object, since the response was ".ok"!
+    showWineList(data);
+  }
+
+  function showWineList(wines) {
+    mainRight.innerHTML = `
+    <div class="list-group">
+    ${wines
+      .map(function (w) {
+        return `<a data-wine-id="${w.id}" class="wine-link list-group-item list-group-item-action ">${w.name}</a>`;
+      })
+      .join("")}
+    <div class="col-sm-5">
+    <button type="button" id="back-btn" class="btn btn-primary btn-sm">back</button>
+    <button type="button" id="forward-btn" class="btn btn-primary btn-sm">forward</button>
+    </div>
+    </div>
+    `;
+    for (let e of mainRight.querySelectorAll(".wine-link")) {
+      e.addEventListener("click", handlesWineLinkClick);
+    }
+    let backBtn = document.querySelector("#back-btn");
+    backBtn.addEventListener("click", handlesBackBtn);
+
+    let forwardBtn = document.querySelector("#forward-btn");
+    forwardBtn.addEventListener("click", handlesForwardBtn);
+  }
+
+  function handlesBackBtn() {
+    pageNumber--;
+    if (pageNumber < 1) {
+      pageNumber = 1;
+    }
+    getAllWines();
+  }
+  function handlesForwardBtn() {
+    pageNumber++;
+    getAllWines();
+  }
+
+  async function handlesWineLinkClick(event) {
+    let wineID = event.target.dataset.wineId;
+    let response = await fetch(apiURL(`/wines/${wineID}`));
+    let data = await response.json();
+    showWine(data);
+  }
+
   function showWine(wine) {
     mainCenter.innerHTML = `
-    <div class="card w-50">
-  <div class="card-body">
+    <div class="wine-info">
     <h5 class="card-title">${wine.name}</h5>
     <h6 class="card-kind">${wine.kind}</h6>
     <h6 class="card-kind">${wine.cost}</h6>
-    <h6 class="card-reg">${wine.reg}</h6>
-    <a href="#" class="btn btn-primary">Back to Wine List</a>
+    <h6 class="card-reg">${wine.region}</h6>
   </div>
 </div>
   `;
   }
 
+  getAllWines();
   showLogin();
 })();
